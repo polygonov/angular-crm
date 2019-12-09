6b26b62a3ac361ec97cbde0cef9ca794a85fa30a
ipolyakov <ipolyakov@bcc.ru>
2019-12-09 18:08:53 +0300

    filter done

--- src/app/system/history-page/history-filter/history-filter.component.ts
+++ src/app/system/history-page/history-filter/history-filter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Category} from '../../../shared/interfaces';
 
 @Component({
   selector: 'app-history-filter',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryFilterComponent implements OnInit {
 
+  @Output() onFilterCancel = new EventEmitter<any>()
+  @Output() onFilterApply = new EventEmitter<any>()
+
+  @Input() categories: Category[] = []
+
+  selectedPeriod = 'd'
+  selectedTypes = []
+  selectedCategories = []
+
   constructor() { }
+
+  timePeriods = [
+    {type: 'd', label: 'День'},
+    {type: 'w', label: 'Неделя'},
+    {type: 'M', label: 'Месяц'}
+  ]
+
+  types = [
+    {type: 'income', label: 'Доход'},
+    {type: 'outcome', label: 'Расход'}
+  ]
 
   ngOnInit() {
   }
 
+  closeFilter() {
+    this.selectedTypes = []
+    this.selectedCategories = []
+    this.selectedPeriod = 'd'
+    this.onFilterCancel.emit()
+  }
+
+  private calculateInputParams(field: string, checked: boolean, value: string) {
+    if (checked) {
+      this[field].indexOf(value) === -1 ? this[field].push(value) : null
+    } else {
+      this[field] = this[field].filter(i => i !== value)
+    }
+  }
+
+  handleChangeType(e) {
+    console.log("e", e)
+    this.calculateInputParams('selectedTypes', e.target.checked, e.target.value)
+  }
+
+  handleChangeCategory(e) {
+    this.calculateInputParams('selectedCategories', e.target.checked, e.target.value)
+  }
+
+  applyFilter() {
+    this.onFilterApply.emit({
+      types: this.selectedTypes,
+      categories: this.selectedCategories,
+      period: this.selectedPeriod
+    })
+  }
 }
--- src/app/system/history-page/history-page.component.ts
+++ src/app/system/history-page/history-page.component.ts
@@ -3,6 +3,7 @@ import {forkJoin, Subscription} from 'rxjs';
 import {Category, CRMEvent} from '../../shared/interfaces';
 import {CategoriesService} from '../shared/services/categories.service';
 import {EventsService} from '../shared/services/events.service';
+import * as moment from 'moment'
 
 @Component({
   selector: 'app-history-page',
@@ -14,8 +15,10 @@ export class HistoryPageComponent implements OnInit, OnDestroy {
   sub: Subscription
   categories: Category[] = []
   events: CRMEvent[] = []
+  filteredEvents: CRMEvent[] = []
   isLoaded = false
   chartData = []
+  isFilterVisible = false
 
   constructor(
     private categoriesService: CategoriesService,
@@ -25,7 +28,7 @@ export class HistoryPageComponent implements OnInit, OnDestroy {
   calculateChartData(): void {
     this.chartData = []
     this.categories.forEach((cat) => {
-      const catEvent = this.events.filter((e) => e.category === cat.id && e.type === 'outcome')
+      const catEvent = this.filteredEvents.filter((e) => e.category === cat.id && e.type === 'outcome')
       this.chartData.push({
         name: cat.name,
         value: catEvent.reduce((total, e) => {
@@ -44,6 +47,7 @@ export class HistoryPageComponent implements OnInit, OnDestroy {
       this.categories = data[0]
       this.events = data[1]
 
+      this.setOriginalEvents()
       this.calculateChartData()
 
       this.isLoaded = true
@@ -57,4 +61,45 @@ export class HistoryPageComponent implements OnInit, OnDestroy {
     }
   }
 
+  private toggleFilterVisibility(dir: boolean) {
+    this.isFilterVisible = dir
+  }
+
+  openFilter() {
+    this.toggleFilterVisibility(true)
+  }
+
+  onFilterApply(filterData: any) {
+    this.toggleFilterVisibility(false)
+    this.setOriginalEvents()
+
+    const startPeriod = moment().startOf(filterData.period).startOf('d')
+    const endPeriod = moment().endOf(filterData.period).endOf('d')
+
+    this.filteredEvents = this.filteredEvents
+      .filter((e) => {
+        return filterData.types.indexOf(e.type) !== -1
+      })
+      .filter((e) => {
+        return filterData.categories.indexOf(e.category.toString()) !== -1
+      })
+      .filter((e) => {
+        const momentDate = moment(e.date, 'DD.MM.YYYY HH:mm:ss')
+        return momentDate.isBetween(startPeriod, endPeriod)
+      })
+
+    this.calculateChartData()
+  }
+
+  onFilterCancel() {
+    this.toggleFilterVisibility(false)
+    this.setOriginalEvents()
+    this.calculateChartData()
+  }
+
+  private setOriginalEvents() {
+    this.filteredEvents = this.events.slice()
+  }
+
 }
+
